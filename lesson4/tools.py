bc3aabefee468e13adb1edb5fdff79c26384403c
tomchen0722 <tomchen0722@gmail.com>
2024-11-23 08:06:15 +0000

    modify

--- lesson4/tools.py
+++ lesson4/tools.py
@@ -2,7 +2,35 @@ PI=3.1415926  #常數要用大寫
 
 
 class Person:
-    pass
+    '''
+    我是Person
+    '''
+    def __init__(self,name:str,age:int):#self可以省略
+        self.name = name #name,age (attribute)
+        self.age = age #name,age (attribute)
+    
+    #實體方法(instance method)
+    def echo(self):
+        print(f'我的姓名是:{self.name}')
+        print(f'我的年齡是:{self.age}')
+class Student(Person):#（子類別) Student繼承->Person繼承->object 
+    '''
+    我是Student
+    '''
+    
+    def __init__(self,name:str,age:int,score:int):
+            super().__init__(name=name,age=age) #super引用父類別
+            self.__score = score #保護score 用__
+
+    @property #只能讀不能做修改
+    def score(self)->int:
+         return self.__score
+    
+    def echo(self):
+        super().echo()#super引用父類別
+        #print(f'我的姓名是:{self.name}')
+        #print(f'我的年齡是:{self.age}')
+        print(f'我的score是:{self.score}')   
 def get_status(bmi:float)->str:
     '''
     docstring
